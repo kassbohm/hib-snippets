fd68802873747b00d6d878691ebffb5cc304b703
kai <kassbohm@gmail.com>
2019-08-29 20:16:35 +0300

    "tag"

---
+++ 1.11.py
@@ -0,0 +1,15 @@
+
+from sympy.physics.units import *
+from sympy import *
+
+t1 = 2 *s
+s1 = 1 *m
+v1 = 2 *m/s
+t2 = 6 *s
+b = 2 *m/s/s/s
+a0 = 1 *m/s/s
+
+v2 = v1 - a0*t2 + b*t2*t2/2
+
+pprint("\nv2 / (m/s):")
+pprint(v2 / (m/s))
