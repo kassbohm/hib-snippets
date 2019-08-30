94ac1df147b0cf1a94e0ae4142502fd5b112b49e
kai <kassbohm@gmail.com>
2019-08-30 11:30:40 +0300

    "tag"

---
+++ 1.15.py
@@ -0,0 +1,17 @@
+# https://colab.research.google.com/drive/1NwUs4TSe5y0dQBHBwI8ORM-ek08PqdTS
+
+from sympy.physics.units import *
+from sympy import *
+
+k = 5 *m*m/s
+b = 4 *m
+
+s2 = var("s2", positive=True)
+eq = Eq( k * 6*s, b*s2 + s2*s2/2 - b *5*m - 12*m*m  )
+
+pprint("\nEquation:")
+pprint(eq)
+
+s2 = solve(eq,s2)[0]
+pprint("\ns2 / m:")
+pprint(N(s2/m,3))
