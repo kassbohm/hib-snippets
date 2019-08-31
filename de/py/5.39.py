c5612c77cefa3bac490fc794f1ced0b81cb93611
kai <kassbohm@gmail.com>
2019-08-31 13:47:07 +0300

    "tag"

--- de/py/5.39.py
+++ de/py/5.39.py
@@ -1,28 +1,23 @@
 from sympy.physics.units import *
 from sympy import *
+from mpmath import radians
 
 prec = 4
 
-r = 3*m/10
+r = 3 * m/10
 v = 2 * m/s
 a = 3 * m/s**2
 
-p1 = rad(50)
-p1 = 50*pi/180
-c1 = cos(p1)
-s1 = sin(p1)
+p1 = radians(50)
+(c1, s1) = (cos(p1), sin(p1))
 
-omega = v/(r*s1)
-pprint("\nomega / (1/s):")
-tmp = omega
+pprint("\nω / (1/s):")
+w = v/(r*s1)
+tmp = w
 tmp /= 1/s
 pprint(N(tmp,prec))
 
-
-pprint("\nalpha / (1/s²):")
-alpha  = N(a/r)
-alpha -= N(omega**2*c1)
-alpha /= s1
-tmp = alpha
+pprint("\nα / (1/s²):")
+tmp  = (a/r - w*w*c1)/s1
 tmp /= (1/s**2)
 pprint(N(tmp,prec))
