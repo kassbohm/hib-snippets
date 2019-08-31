a323a32cc0f8db6f1be504a18e71f26ab17e7471
kai <kassbohm@gmail.com>
2019-08-31 16:19:40 +0300

    "tag"

---
+++ de/py/6.34.py
@@ -0,0 +1,62 @@
+from sympy.physics.units import *
+from sympy import *
+
+newton = kg*m/s**2
+g = S(981)/100 *m/s**2
+
+mass, a, mu = var("mass, a, mu")
+
+prec = 5
+
+sub_list = [
+    (mass, 800 *kg),
+    (a, S(5)/10 *m/s**2),
+    (mu, S(1)/10),
+    ]
+
+G = mass*g
+c = sqrt(2)/2
+
+Nv, S = var("Nv, S")
+eq1 = Eq(mass*a, S*c - mu*Nv)
+eq2 = Eq(G, Nv + S*c)
+sol = solve([eq1, eq2],[Nv, S])
+
+pprint("\nN / Newton:")
+Nv = sol[Nv]
+tmp = Nv
+tmp = tmp.subs(sub_list)
+tmp = tmp.simplify()
+tmp /= newton
+pprint(N(tmp,prec))
+
+pprint("\nS / Newton:")
+S = sol[S]
+tmp = S
+tmp = tmp.subs(sub_list)
+tmp = tmp.simplify()
+tmp /= newton
+pprint(N(tmp,prec))
+
+pprint("\nphi / deg:")
+phi = asin(mu * Nv / S)
+tmp = phi
+tmp = tmp.subs(sub_list)
+tmp = tmp.simplify()
+tmp = deg(tmp)
+pprint(N(tmp,prec))
+pprint("\ntheta / deg:")
+tmp = 45 - tmp
+pprint(N(tmp,prec))
+
+# N / Newton:
+# 6770.9
+#
+# S / Newton:
+# 1523.2
+#
+# phi / deg:
+# 26.392
+#
+# theta / deg:
+# 18.608
