57864d3d004cd1f3192e0572408bbdfd76800986
kai <kassbohm@gmail.com>
2019-09-01 11:18:51 +0300

    "tag"

---
+++ de/py/9.ex2.py
@@ -0,0 +1,23 @@
+from sympy.physics.units import *
+from sympy import *
+
+(wS, wSp) = ( 10 /s, 6 /s**2 )
+(wN, wNp) = (  3 /s, 2 /s**2 )
+(wP, wPp) = (  5 /s, 4 /s**2 )
+
+theta = 60 *pi/180
+(ct, st) = (cos(theta), sin(theta))
+
+w = Matrix([-wN, wS*st, wP + wS*ct])
+
+pprint("\nω / (1/s):")
+tmp = w / (1/s)
+pprint(N(tmp,3))
+
+wp  = Matrix([-wNp, wSp*st, wSp*ct + wPp])
+O = Matrix([-wN, 0, wP])
+wp += O.cross(w) + O.cross(Matrix([-wN, 0, 0]))
+
+pprint("\nω' / (1/s²):")
+tmp = wp / (1/s/s)
+pprint(N(tmp,3))
