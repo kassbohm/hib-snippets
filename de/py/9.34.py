c780e2186232e6be2a5e1e63fd86e441575a4ca1
kai <kassbohm@gmail.com>
2019-09-01 15:44:10 +0300

    "tag"

---
+++ de/py/9.34.py
@@ -0,0 +1,68 @@
+from sympy.physics.units import *
+from sympy import *
+
+( b, c, d ) = ( S(5)/4 *m, 1 *m, S(7)/4 *m)
+
+# Omega and Omega':
+O  = Matrix([0, 0, 5]) / s
+Op = Matrix([0, 0, 2]) / (s**2)
+w  = Matrix([0, 2, 0]) / s
+wp = Matrix([0, 1, 0]) / (s**2)
+
+dAB = Matrix([-b, 0, 0])
+dBC = Matrix([0, d, c])
+
+vB = O.cross(dAB)
+aB = Op.cross(dAB) + O.cross(O.cross(dAB))
+
+# units:
+vu = m/s
+au = m/(s**2)
+
+# precision:
+prec = 10
+
+pprint("\nvB / (m/s):")
+pprint(N(vB/vu,prec))
+pprint("\naB / (m/s²):")
+pprint(N(aB/au,prec))
+
+vCB = w.cross(dBC)
+aCB = wp.cross(dBC) + w.cross(w.cross(dBC))
+
+vC = vB + O.cross(dBC) + vCB
+aC = aB + Op.cross(dBC) + O.cross(O.cross(dBC)) + 2*O.cross(vCB) + aCB
+
+pprint("\nvC / (m/s):")
+pprint(N(vC/vu,prec))
+pprint("\naC / (m/s²):")
+pprint(N(aC/au,prec))
+
+
+# vB / (m/s):
+# ⎡  0  ⎤
+# ⎢     ⎥
+# ⎢-6.25⎥
+# ⎢     ⎥
+# ⎣  0  ⎦
+#
+# aB / (m/s²):
+# ⎡31.25⎤
+# ⎢     ⎥
+# ⎢-2.5 ⎥
+# ⎢     ⎥
+# ⎣  0  ⎦
+#
+# vC / (m/s):
+# ⎡-6.75⎤
+# ⎢     ⎥
+# ⎢-6.25⎥
+# ⎢     ⎥
+# ⎣  0  ⎦
+#
+# aC / (m/s²):
+# ⎡28.75 ⎤
+# ⎢      ⎥
+# ⎢-26.25⎥
+# ⎢      ⎥
+# ⎣ -4.0 ⎦
